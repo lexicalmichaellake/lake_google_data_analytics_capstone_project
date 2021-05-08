507579991f13c383d4ffbc0bb13e79f4a861a8b3
William Michael Lake <65189195+lexicalmichaellake@users.noreply.github.com>
2021-05-08 17:07:09 -0400

    Add files via upload

---
+++ caption_downloader_converter_2.py
@@ -0,0 +1,34 @@
+import glob
+import re
+import shutil
+import string
+import os
+from pytube import YouTube
+
+global wd
+global curr_directory
+curr_directory = os.getcwd()
+global wd
+wd = os.path.dirname(os.path.abspath(__file__))
+print(wd)
+
+#modify this URL list with the video ids you wish to download.
+url_list = ["Rql--9SOMC8", "zUXwqYxKkN0"]
+
+
+j = 0
+
+for i in url_list:
+	link = 'https://www.youtube.com/watch?v=' + str(i)
+	src = YouTube(link)
+	# Getting only English captions by specifying 'en' as parameter
+	a_en_caption = src.captions.get_by_language_code('a.en')
+	print(a_en_caption.xml_captions)
+  	# Instead of Captions in XML format we are converting it to string format.
+	en_caption_convert_to_srt = 		(a_en_caption.generate_srt_captions())
+	# print(en_caption_convert_to_srt)
+	f = open( curr_directory + "/" + str(i) + ".txt", 'w', encoding="utf8")		
+	f.truncate()
+	f.write(en_caption_convert_to_srt)
+	f.close()
+	j = j + 1
